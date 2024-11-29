8d9646fcd28e592c6e4695bd4d3114e5159a6a48
wxb <wxb1@h-sun.com>
2024-11-29 15:43:32 +0800

    更新licales方言导入报错

--- src/views/Info/PackageInfo.vue
+++ src/views/Info/PackageInfo.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { ref,reactive,computed } from 'vue';
 import { packageListApi,bomListApi,selectDownload,queryDownload} from '@/api/mo';
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-import en from 'element-plus/dist/locale/en.mjs'
+import { zhCn,en } from 'element-plus/es/locales.mjs';
 
 interface TableRow {
   id: number;  // 假设 id 是数字类型，如果是其他类型请修改
@@ -186,6 +185,7 @@ const queryContentDownload = async () => {
 
 <template>
     <div>
+        <el-config-provider :locale="locale">
         <div>
             <el-form :inline="true" :model="queryForm" class="demo-form-inline">
                 <el-form-item label="芯片名称">
@@ -267,132 +267,131 @@ const queryContentDownload = async () => {
                 </template>
             下载已选择
             </el-button>
-            <el-config-provider :locale="locale">
-                <el-table 
-                    :data="assyHistoryData" 
-                    v-loading="loading" 
-                    :header-cell-style="headerRowStyle" 
-                    @selection-change="handleSelectionChange"
-                    border 
-                    lazy  
-                    row-key="order_id"
-                    @expand-change="handleExpandChange"
-                    highlight-current-row 
-                    height="640" 
-                    style="width: 100%;">
-                    <el-table-column type="expand">
-                        <template #default="props">
-                            <div class="ml-5 w-500">
-                                <!-- <el-table :data="props.row.children" border lazy :header-cell-style="headerRowStyle">
-                                    <el-table-column prop="main_chip" label="主芯" align="center" width="80" show-overflow-tooltip />
-                                    <el-table-column prop="bom_code" label="物料编码" align="center" width="350" show-overflow-tooltip  />
-                                    <el-table-column prop="item_name" label="物料名称" align="center" width="200" show-overflow-tooltip />
-                                    <el-table-column prop="bom_lot" label="批号" width="150" align="center" show-overflow-tooltip />
-                                    <el-table-column prop="bom_business_qty" label="Die数" align="center" width="120" />
-                                    <el-table-column prop="bom_second_qty" label="数量" width="60" align="center" show-overflow-tooltip />
-                                    <el-table-column prop="bom_wafer_id" label="片号" width="680" show-overflow-tooltip />
-                                </el-table> -->
-                                <div v-for="item in props.row.children" :key="item.id" style="margin-bottom: 20px;width: 80%;">
-                                    <el-descriptions 
-                                        class="margin-top"
-                                        :title="item.main_chip"
-                                        :column="4"
-                                        border
-                                    >
-                                    <el-descriptions-item label-align="center">
-                                        <template #label>
-                                            <div class="cell-item">
-                                                物料编码
-                                            </div>
-                                        </template>
-                                        {{item.bom_code}}
-                                    </el-descriptions-item>
+            <el-table 
+                :data="assyHistoryData" 
+                v-loading="loading" 
+                :header-cell-style="headerRowStyle" 
+                @selection-change="handleSelectionChange"
+                border 
+                lazy  
+                row-key="order_id"
+                @expand-change="handleExpandChange"
+                highlight-current-row 
+                height="640" 
+                style="width: 100%;">
+                <el-table-column type="expand">
+                    <template #default="props">
+                        <div class="ml-5 w-500">
+                            <!-- <el-table :data="props.row.children" border lazy :header-cell-style="headerRowStyle">
+                                <el-table-column prop="main_chip" label="主芯" align="center" width="80" show-overflow-tooltip />
+                                <el-table-column prop="bom_code" label="物料编码" align="center" width="350" show-overflow-tooltip  />
+                                <el-table-column prop="item_name" label="物料名称" align="center" width="200" show-overflow-tooltip />
+                                <el-table-column prop="bom_lot" label="批号" width="150" align="center" show-overflow-tooltip />
+                                <el-table-column prop="bom_business_qty" label="Die数" align="center" width="120" />
+                                <el-table-column prop="bom_second_qty" label="数量" width="60" align="center" show-overflow-tooltip />
+                                <el-table-column prop="bom_wafer_id" label="片号" width="680" show-overflow-tooltip />
+                            </el-table> -->
+                            <div v-for="item in props.row.children" :key="item.id" style="margin-bottom: 20px;width: 80%;">
+                                <el-descriptions 
+                                    class="margin-top"
+                                    :title="item.main_chip"
+                                    :column="4"
+                                    border
+                                >
+                                <el-descriptions-item label-align="center">
+                                    <template #label>
+                                        <div class="cell-item">
+                                            物料编码
+                                        </div>
+                                    </template>
+                                    {{item.bom_code}}
+                                </el-descriptions-item>
 
-                                    <el-descriptions-item label-align="center">
-                                        <template #label>
-                                            <div class="cell-item">
-                                                物料名称
-                                            </div>
-                                        </template>
-                                        {{item.item_name}}
-                                    </el-descriptions-item>
+                                <el-descriptions-item label-align="center">
+                                    <template #label>
+                                        <div class="cell-item">
+                                            物料名称
+                                        </div>
+                                    </template>
+                                    {{item.item_name}}
+                                </el-descriptions-item>
 
-                                    <el-descriptions-item label-align="center">
-                                        <template #label>
-                                            <div class="cell-item">
-                                                片数
-                                            </div>
-                                        </template>
-                                        {{item.bom_second_qty}}
-                                    </el-descriptions-item>
+                                <el-descriptions-item label-align="center">
+                                    <template #label>
+                                        <div class="cell-item">
+                                            片数
+                                        </div>
+                                    </template>
+                                    {{item.bom_second_qty}}
+                                </el-descriptions-item>
 
-                                    <el-descriptions-item label-align="center">
-                                        <template #label>
-                                            <div class="cell-item">
-                                                Die数
-                                            </div>
-                                        </template>
-                                        {{item.bom_business_qty}}
-                                    </el-descriptions-item>
-                                    
-                                    <el-descriptions-item label-align="center">
-                                        <template #label>
-                                            <div class="cell-item">
-                                                批号
-                                            </div>
-                                        </template>
-                                        {{item.bom_lot}}
-                                    </el-descriptions-item>
+                                <el-descriptions-item label-align="center">
+                                    <template #label>
+                                        <div class="cell-item">
+                                            Die数
+                                        </div>
+                                    </template>
+                                    {{item.bom_business_qty}}
+                                </el-descriptions-item>
+                                
+                                <el-descriptions-item label-align="center">
+                                    <template #label>
+                                        <div class="cell-item">
+                                            批号
+                                        </div>
+                                    </template>
+                                    {{item.bom_lot}}
+                                </el-descriptions-item>
 
-                                    <el-descriptions-item label-align="center" width="100" span="2.5">
-                                        <template #label>
-                                            <div class="cell-item">
-                                                片号
-                                            </div>
-                                        </template>
-                                        {{item.bom_wafer_id}}
-                                    </el-descriptions-item>
+                                <el-descriptions-item label-align="center" width="100" span="2.5">
+                                    <template #label>
+                                        <div class="cell-item">
+                                            片号
+                                        </div>
+                                    </template>
+                                    {{item.bom_wafer_id}}
+                                </el-descriptions-item>
 
-                                    </el-descriptions>
-                                </div>
+                                </el-descriptions>
                             </div>
-                        </template>
-                    </el-table-column>
-                    <el-table-column type="selection" width="50" align="center" />
-                    <el-table-column prop="id" label="序号" width="80" align="center" sortable  />
-                    <el-table-column prop="order_id" label="订单号" width="150" sortable align="center" show-overflow-tooltip />
-                    <el-table-column prop="item_name" label="芯片名称" width="250" align="right" show-overflow-tooltip />
-                    <el-table-column prop="package" label="封装形式" width="100" show-overflow-tooltip />
-                    <el-table-column prop="lot_code" label="打印批号" width="150" show-overflow-tooltip />
-                    <el-table-column prop="business_qty" label="订单数量" width="100" align="right"/>
-                    <el-table-column prop="arrive_qty" label="来料数量" width="100" align="right"/>
-                    <el-table-column prop="remark" label="备注" width="100" show-overflow-tooltip align="left" />
-                    <el-table-column prop="order_date" label="订单日期" width="110" sortable align="center" />
-                    
-                    
-                    <el-table-column prop="assy_step" label="加工类型" width="150" show-overflow-tooltip align="center"/>
-                    <el-table-column prop="pgm_name" label="成测程序" width="100" show-overflow-tooltip align="right" />
-                    <el-table-column prop="loading_method" label="装片方式" width="90" show-overflow-tooltip/>
-                    <el-table-column prop="bonding" label="打线图号" width="150" show-overflow-tooltip/>
-                    <el-table-column prop="wire" label="线材" width="100" align="right" show-overflow-tooltip/>
-                    <el-table-column prop="package_remark" label="特殊备注" width="100" show-overflow-tooltip />
-                    <el-table-column prop="complete_date" label="结束日期" width="110" sortable align="center" />               
-                    <el-table-column prop="supply" label="封装厂" show-overflow-tooltip fixed="right" width="100" align="center" />
-                </el-table>
-                <div class="mt-3">
-                    <el-pagination
-                        v-model:current-page="currentPage"
-                        v-model:page-size="pagesize"
-                        :page-sizes="[15, 20, 30, 40, 50]"
-                        size="default"
-                        layout="total, sizes, prev, pager, next, jumper"
-                        :total='totalPage'
-                        :background=true
-                        @size-change="handleSizeChange"
-                        @current-change="handleCurrentChange" />
-                </div>
-            </el-config-provider>
+                        </div>
+                    </template>
+                </el-table-column>
+                <el-table-column type="selection" width="50" align="center" />
+                <el-table-column prop="id" label="序号" width="80" align="center" sortable  />
+                <el-table-column prop="order_id" label="订单号" width="150" sortable align="center" show-overflow-tooltip />
+                <el-table-column prop="item_name" label="芯片名称" width="250" align="right" show-overflow-tooltip />
+                <el-table-column prop="package" label="封装形式" width="100" show-overflow-tooltip />
+                <el-table-column prop="lot_code" label="打印批号" width="150" show-overflow-tooltip />
+                <el-table-column prop="business_qty" label="订单数量" width="100" align="right"/>
+                <el-table-column prop="arrive_qty" label="来料数量" width="100" align="right"/>
+                <el-table-column prop="remark" label="备注" width="100" show-overflow-tooltip align="left" />
+                <el-table-column prop="order_date" label="订单日期" width="110" sortable align="center" />
+                
+                
+                <el-table-column prop="assy_step" label="加工类型" width="150" show-overflow-tooltip align="center"/>
+                <el-table-column prop="pgm_name" label="成测程序" width="100" show-overflow-tooltip align="right" />
+                <el-table-column prop="loading_method" label="装片方式" width="90" show-overflow-tooltip/>
+                <el-table-column prop="bonding" label="打线图号" width="150" show-overflow-tooltip/>
+                <el-table-column prop="wire" label="线材" width="100" align="right" show-overflow-tooltip/>
+                <el-table-column prop="package_remark" label="特殊备注" width="100" show-overflow-tooltip />
+                <el-table-column prop="complete_date" label="结束日期" width="110" sortable align="center" />               
+                <el-table-column prop="supply" label="封装厂" show-overflow-tooltip fixed="right" width="100" align="center" />
+            </el-table>
+            <div class="mt-3">
+                <el-pagination
+                    v-model:current-page="currentPage"
+                    v-model:page-size="pagesize"
+                    :page-sizes="[15, 20, 30, 40, 50]"
+                    size="default"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total='totalPage'
+                    :background=true
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange" />
+            </div>
         </div>
+        </el-config-provider>
     </div>
 </template>
 
